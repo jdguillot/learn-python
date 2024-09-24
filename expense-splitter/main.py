0eb8deb0436f242227718d6d87e6bb90d44bacb2
jdguillot <jdguillot@outlook.com>
2024-09-23 21:18:35 -0700

    Cleaned up the program a little.

--- expense-splitter/main.py
+++ expense-splitter/main.py
@@ -1,4 +1,4 @@
-def calculate_split(total_amount: float, number_of_people: int, split_array: list, currency: str) -> None:
+def calculate_split(total_amount: float, number_of_people: int, split_array: list[float], currency: str) -> None:
     if number_of_people < 1:
         raise ValueError('Number of people must be greater than one.')
     if (sum(split_array) > 100):
@@ -13,33 +13,25 @@ def calculate_split(total_amount: float, number_of_people: int, split_array: lis
         share_per_person: float = total_amount *  (split_array[x] / 100)
         print(f' Person {x + 1} should pay: {currency}{share_per_person:,.2f}')
 
-def user_validation(message: str, val_type: str = 'str'):
+def user_validation(message: str, expected_type: type):
     while True:
         value = input(message)
         try:
-            if (val_type == 'int'):
-                value = int(value)
-                return value
-            elif (val_type == 'float'):
-                value = float(value)
-                return value
-            elif (val_type == 'float_list'):
-                user_list: list = value.split()
-                split_array: list[float] = [float(item) for item in user_list]
-                return split_array
+            if (expected_type == list):
+                user_list: list[str] = value.split()
+                value = [float(item) for item in user_list] if user_list else []
             else:
-                return value
-        except ValueError as e:
-            print(f'"{value}" is not a valid input. Please enter a valid value.')
+                value = expected_type(value)
+            return value
+        except (ValueError, TypeError):
+            print(f'"{value}" is not a valid {expected_type.__name__}. Please try again.')
 
 
 def main() -> None:
     try:
-        total_amount: float = float(user_validation('Enter the total amount of the expense: ', 'float'))
-        number_of_people: int = int(user_validation('Enter the number of people sharing the expense: ', 'int'))
-        split_array: list = user_validation('Enter the split amount for each person seperated by a space (For even split, leave blank): ','float_list')
-        # user_list: list = split_input.split()
-        # split_array: list = [int(item) for item in user_list]
+        total_amount: float = float(user_validation('Enter the total amount of the expense: ', float)) # type: ignore
+        number_of_people: int = int(user_validation('Enter the number of people sharing the expense: ', int)) # type: ignore
+        split_array: list = user_validation('Enter the split amount for each person seperated by a space (For even split, leave blank): ', list)
         split_sum: float = sum(split_array)
         split_num: float = len(split_array)
 
